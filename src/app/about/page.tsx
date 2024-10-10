9ababcd288a01239e3ee40e3754695828a160e7e
Uros Andjelic <uros.andjelic@nabis.com>
2024-10-10 19:35:28 +0200

    add fetch from github as some dynamic thing

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,3 +1,16 @@
-export default function AboutPage() {
-  return <div>This is just a test page for now to see how slow Next.js is.</div>;
+export default async function AboutPage() {
+  const response = await fetch('https://api.github.com/repos/vercel/next.js', {
+    next: {
+      revalidate: 1,
+    },
+  });
+
+  const data = await response.json();
+  const id = data.id;
+  return (
+    <div>
+      This is just a test page for now to see how slow Next.js is.
+      <div>Here is a vercel id from github: {id}</div>
+    </div>
+  );
 }
